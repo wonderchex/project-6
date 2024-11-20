f402e1a15512b4dbf87e43da4081ebf705131f25
Spyro1001 <raymondhedgecock@gmail.com>
2024-11-19 17:25:53 -0700

    Initial commit

---
+++ dictionary/src/test/java/com/example/dictionary/DictionaryApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+public class DictionaryApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(applicationContext, "The application context should have loaded.");
+    }
+}
